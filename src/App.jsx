4b39e470d9dfdf96f917a4dc319357b191b79942
Caio Luan <157083928+caioluanmatos@users.noreply.github.com>
2026-07-16 13:44:10 -0300

    feat: configura React Router e estrutura inicial de rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import  Login from './pages/Login/Login'
 import './App.css'
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
 
   return (
     <>
-      <Login />
+     <AppRoutes/>
     </>
   )
 }
