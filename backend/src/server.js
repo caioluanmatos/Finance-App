c313af4055e53b96790c97c5796d28d984771027
Caio Luan <157083928+caioluanmatos@users.noreply.github.com>
2026-07-26 15:17:29 -0300

    feat: adiciona rota de login

--- backend/src/server.js
+++ backend/src/server.js
@@ -34,3 +34,25 @@ app.post("/cadastro", (req, res) => {
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000");
 });
+
+
+app.post("/login", (req, res) => {
+    const { email, senha } = req.body;
+
+    const sql =
+        "SELECT * FROM usuarios WHERE email = ? AND senha = ?";
+
+    connection.query(sql, [email, senha], (error, result) => {
+
+        if (error) {
+            console.log(error);
+            return res.status(500).send("Erro ao fazer login");
+        }
+
+        if (result.length === 0) {
+            return res.send("E-mail ou senha inválidos!");
+        }
+
+        res.send("Login realizado com sucesso!");
+    });
+});
